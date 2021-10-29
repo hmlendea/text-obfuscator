90c593e18dc1262541974c69ee5701895716ed3d
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-29 13:35:22 +0300

    More mappings

--- js/text-obfuscator.js
+++ js/text-obfuscator.js
@@ -1,21 +1,45 @@
 var identicalReplacements = {
+    "3": "З",
+
     "A": "А",
-    "Đ": "Ɖ",
+    "Ă": "Ă",
+    "B": "В",
+    "C": "С",
+    "Ð": "ĐƉ",
+    "Đ": "ÐƉ",
+    "Ɖ": "ÐĐ",
     "E": "ЕΕ",
-    "I": "ІΙ",
+    "Ë": "Ё",
+    "Ĕ": "Ӗ",
+    "H": "Н",
+    "I": "ІΙӀӏ",
     "J": "Ј",
     "K": "К",
     "Ḱ": "Ќ",
+    "M": "М",
     "O": "ОΟ",
+    "Ӧ": "Ö",
+    "P": "Р",
+    "X": "Х",
     "T": "Т",
+    "Y": "Ү",
 
     "a": "а",
+    "ă": "ӑ",
+    "c": "с",
     "e": "е",
+    "ë": "ё",
+    "ĕ": "ӗ",
     "i": "і",
     "j": "ј",
     "o": "о",
+    "ӧ": "ö",
     "ó": "ό",
-    "ú": "ύ"
+    "p": "р",
+    "ú": "ύ",
+    "x": "х",
+    "y": "у",
+    "ÿ": "ӱ"
 }
 
 var characters = lowercase + uppercase + digits + punctuation;
@@ -27,14 +51,21 @@ function obfuscateText() {
 
     for (var i = 0; i < input.length; i++) {
         var character = input[i];
+        var wasObfuscated = false;
 
-        if (!identicalReplacements[character]) {
+        // 40% chance to obfuscate
+        if (Math.floor(Math.random() * 10) + 1 <= 4) {
+            if (identicalReplacements[character]) {
+                var candidates = identicalReplacements[character];
+                var randomCandidateIndex = Math.floor(Math.random() * candidates.length);
+    
+                output += candidates[randomCandidateIndex];
+                wasObfuscated = true;
+            }
+        }
+        
+        if (!wasObfuscated) {
             output += input[i];
-        } else {
-            var candidates = identicalReplacements[character];
-            var randomCandidateIndex = Math.floor(Math.random() * candidates.length);
-
-            output += candidates[randomCandidateIndex];
         }
     }
 
